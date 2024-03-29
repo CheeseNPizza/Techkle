3fe6842571c707c0af01877914765de015f85e65
jiasyuen <jiasyuen1426@1utar.my>
2024-03-29 22:23:55 +0800

    footer

---
+++ footer.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <!-- <link rel="stylesheet" type="text/css" href="css/main_header.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>  -->
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+            font-family: "Poppins", sans-serif;
+        }
+
+        .footer-container {
+            width: 100%;
+            align-items: center;
+            background-color: #fff;
+            position: relative;
+            bottom: 0;
+            height: 250px;
+        }
+
+        .footer-content {
+            display: flex;
+            flex-direction:column;
+            align-items: center;
+            padding: 50px 0 0 0;
+            color: #005691;
+        }
+
+        .footer-content h1 {
+            font-size: 42px;
+        }
+
+        .footer-content p {
+            font-size: 20px;
+        }
+
+        .line {
+            width: 90%;
+            height: 3px;
+            background-color: #005691;
+            margin: 10px 0;
+        }
+
+    </style>
+</head>
+<body>
+    <footer>
+        <div class="footer-container">
+            <div class="footer-content">
+                <h1>Techkle</h1>
+                <div class="line"></div>
+                <p>© 2024 Techkle, All Rights Reserved.</p>
+            </div>
+        </div>
+    </footer>
+</body>
+</html>
